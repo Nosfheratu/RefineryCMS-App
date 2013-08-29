756a697f55c0899927200100b7795b1db2528bdb
Fernando Serapio <fhernandosc@gmail.com>
2013-08-29 19:40:51 +0000

    Created application using Refinery CMS 2.1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '27f95d340e5f0fb218f837e29a99b2d636df7c29f8c22bc74bf732bb8e1174c6a2b97f9d179389d8d4cb8e71b7e4300b1930198026275211112d07cf2769179c'
